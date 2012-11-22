9e670e9a11d8211983f247081be6278def28b2d0
sasaplus1 <sasaplus1@gmail.com>
2012-11-23 00:19:22 +0900

    Closure Linterに沿うようにした

--- base62.js
+++ base62.js
@@ -1,22 +1,18 @@
-/**
- * @license base62.js Copyright(c) 2012 sasa+1
- * Released under the MIT license
- * http://github.com/sasaplus1/base62.js/blob/master/LICENSE
- */
-;(function(){
+// base62.js Copyright(c) 2012 sasa+1
+// Released under the MIT license
+// http://github.com/sasaplus1/base62.js/blob/master/LICENSE
 
-  var Base62 = (function(){
+;(function() {
+
+  var Base62 = (function() {
 
     var TABLES_ = {
-      '09azAZ': '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
-      '09AZaz': '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+      '09azAZ':
+          '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+      '09AZaz':
+          '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
     }, tableKey_ = '09azAZ';
 
-    /**
-     * 変換に使用するテーブルを切り替えます。
-     * @param {string} tableKey テーブルのキー
-     * @throws {Error} 存在しないキーが指定された場合
-     */
     function changeTable_(tableKey) {
       if (typeof tableKey !== 'string' || TABLES_[tableKey] === void 0) {
         tableKey_ = '';
@@ -25,11 +21,6 @@
       tableKey_ = tableKey;
     }
 
-    /**
-     * 62進数を10進数に変換します。
-     * @param {string} str 62進数の文字列
-     * @return {number} 10進数に変換された数値
-     */
     function decode_(str) {
       var decodedNum = 0,
           column, len, isNegative, table;
@@ -47,7 +38,7 @@
       column = len = str.length;
       for (; column--;) {
         decodedNum +=
-          table.indexOf(str[column]) * Math.pow(62, len - column - 1);
+            table.indexOf(str[column]) * Math.pow(62, len - column - 1);
       }
 
       if (isNegative) {
@@ -57,11 +48,6 @@
       return decodedNum;
     }
 
-    /**
-     * 10進数を62進数に変換します。
-     * @param {number} num 10進数の数値
-     * @return {string} 62進数に変換された文字列
-     */
     function encode_(num) {
       var encodedStr, isNegative, table;
 
@@ -90,10 +76,6 @@
       return encodedStr;
     }
 
-    /**
-     * 現在設定されている変換テーブルを返します。
-     * @return {string} 変換テーブル
-     */
     function getTable_() {
       return TABLES_[tableKey_];
     }
