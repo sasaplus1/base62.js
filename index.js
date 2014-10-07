30b1b85c5fc75eea99ef217c182e87a544ea9add
sasaplus1 <sasaplus1@gmail.com>
2014-10-07 22:25:52 +0900

    splitted libs

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 /*!
- * base62.js Copyright(c) 2012 sasa+1
+ * base62.js Copyright(c) 2012-2014 sasa+1
  * https://github.com/sasaplus1/base62.js
  * Released under the MIT license.
  */
 
 
-/** export ./lib/base62.js. */
-module.exports = require('./lib/base62');
+var base62 = require('./lib/base62'),
+    utility = require('./lib/utility');
+
+
+/**
+ * export.
+ */
+if (process.env.NODE_ENV === 'test') {
+  base62.isInteger = function() {
+    return utility.isInteger.apply(utility, arguments);
+  };
+}
+
+module.exports = base62;
