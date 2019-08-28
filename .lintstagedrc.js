3bd000913c5180149d3a7357b239eb2976df2c8c
sasaplus1 <sasaplus1@gmail.com>
2019-08-29 00:01:44 +0900

    update .lintstagedrc.js

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,3 +1,13 @@
 module.exports = {
+  '*.(js|ts)': 'npx eslint --cache',
+  '*.yml': [
+    'npx prettier --parser yaml --write',
+    'git diff --exit-code --quiet'
+  ],
+  'package.json': [
+    'npx fixpack',
+    'npx prettier --parser json-stringify --write',
+    'git diff --exit-code --quiet'
+  ],
   'package-lock.json': 'node -e "process.exit(1);"'
 };
