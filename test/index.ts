f1c95979813f9b3a04522b5ae76b5762c071383c
sasaplus1 <sasaplus1@gmail.com>
2019-08-29 02:02:33 +0900

    update tests

--- test/index.ts
+++ test/index.ts
@@ -1,166 +1,139 @@
-var expect = this.expect || require('expect.js'),
-    base62 = this.base62 || require('../');
+import assert = require('assert');
+
+import { decode, encode } from '../index';
 
 describe('base62', function() {
+  const originalTable =
+    '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
 
-  describe('.Base62()', function() {
-
-    it('should threw error if param is not a string', function() {
-      function f(v) {
-        return function() {
-          new base62.Base62(v);
-        };
-      }
-
-      function fn(e) {
-        expect(e).to.be.a(TypeError);
-      }
-
-      expect(f(1)).to.throwError(fn);
-      expect(f(true)).to.throwError(fn);
-      expect(f(null)).to.throwError(fn);
-      expect(f(void 0)).to.throwError(fn);
-      expect(f(function() {})).to.throwError(fn);
-    });
-
-    it('should threw error if param is not 62 chars', function() {
-      function f(v) {
-        return function() {
-          new base62.Base62(v);
-        };
-      }
-
-      function fn(e) {
-        expect(e).to.be.a(Error);
-      }
-
-      expect(
-          f('61-----------------------------------------------------------')
-      ).to.throwError(fn);
-      expect(
-          f('63-------------------------------------------------------------')
-      ).to.throwError(fn);
-    });
-
-    it('should has default table', function() {
-      expect((new base62.Base62).table_).to.be(
-          '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
-    });
-
-    it('should set another table', function() {
-      var t = '62------------------------------------------------------------',
-          b = new base62.Base62(t);
-
-      expect(b.table_).to.be(t);
-    });
-
-  });
-
-  describe('.createConverter()', function() {
-
-    it('should returned Base62 instance', function() {
-      expect(base62.createConverter()).to.be.a(base62.Base62);
-    });
-
-  });
-
-  describe('#decode()', function() {
-
+  describe('decode()', function() {
     it('should threw error if param type is not a string', function() {
-      function f(v) {
-        return function() {
-          base62.decode(v);
-        };
+      /* eslint-disable @typescript-eslint/no-explicit-any, @typescript-eslint/no-empty-function */
+      assert.throws(function() {
+        decode(1 as any);
+      });
+      assert.throws(function() {
+        decode(true as any);
+      });
+      assert.throws(function() {
+        decode(null as any);
+      });
+      assert.throws(function() {
+        decode(undefined as any);
+      });
+      assert.throws(function() {
+        decode(function() {} as any);
+      });
+      if (typeof Symbol !== 'undefined') {
+        assert.throws(function() {
+          decode(Symbol() as any);
+        });
       }
-
-      function fn(e) {
-        expect(e).to.be.a(TypeError);
-      }
-
-      expect(f(1)).to.throwError(fn);
-      expect(f(true)).to.throwError(fn);
-      expect(f(null)).to.throwError(fn);
-      expect(f(void 0)).to.throwError(fn);
-      expect(f(function() {})).to.throwError(fn);
+      /* eslint-enable @typescript-eslint/no-explicit-any, @typescript-eslint/no-empty-function */
     });
 
     it('should threw error if param is not an integer', function() {
-      function f(v) {
-        return function() {
-          base62.decode(v);
-        };
-      }
+      assert.throws(function() {
+        decode('');
+      });
+      assert.throws(function() {
+        decode('-');
+      });
+      assert.throws(function() {
+        decode('0.1');
+      });
+    });
 
-      function fn(e) {
-        expect(e).to.be.a(Error);
-      }
-
-      expect(f('')).to.throwError(fn);
-      expect(f('-')).to.throwError(fn);
-      expect(f('0.1')).to.throwError(fn);
+    it('should threw error if baseTable is not 62 in length', function() {
+      assert.throws(function() {
+        decode('0', '0123456789');
+      });
     });
 
     it('should convert to integer from base62 string', function() {
-      expect(base62.decode('0')).to.be(0);
-      expect(base62.decode('Z')).to.be(61);
-      expect(base62.decode('10')).to.be(62);
-      expect(base62.decode('-0')).to.be(0);
-      expect(base62.decode('-Z')).to.be(-61);
-      expect(base62.decode('-10')).to.be(-62);
-      expect(base62.decode('2lkCB1')).to.be(2147483647);
-      expect(base62.decode('-2lkCB2')).to.be(-2147483648);
+      assert(decode('0') === 0);
+      assert(decode('Z') === 61);
+      assert(decode('10') === 62);
+      assert(decode('-0') === 0);
+      assert(decode('-Z') === -61);
+      assert(decode('-10') === -62);
+      assert(decode('2lkCB1') === 2147483647);
+      assert(decode('-2lkCB2') === -2147483648);
     });
 
+    it('should convert to integer from base62 string with original table', function() {
+      assert(decode('0', originalTable) === 0);
+      assert(decode('z', originalTable) === 61);
+      assert(decode('10', originalTable) === 62);
+      assert(decode('-0', originalTable) === 0);
+      assert(decode('-z', originalTable) === -61);
+      assert(decode('-10', originalTable) === -62);
+      assert(decode('2LKcb1', originalTable) === 2147483647);
+      assert(decode('-2LKcb2', originalTable) === -2147483648);
+    });
   });
 
-  describe('#encode()', function() {
-
+  describe('encode()', function() {
     it('should threw error if param type is not a number', function() {
-      function f(v) {
-        return function() {
-          base62.encode(v);
-        };
+      /* eslint-disable @typescript-eslint/no-explicit-any, @typescript-eslint/no-empty-function */
+      assert.throws(function() {
+        encode('a' as any);
+      });
+      assert.throws(function() {
+        encode(true as any);
+      });
+      assert.throws(function() {
+        encode(null as any);
+      });
+      assert.throws(function() {
+        encode(undefined as any);
+      });
+      assert.throws(function() {
+        encode(function() {} as any);
+      });
+      if (typeof Symbol !== 'undefined') {
+        assert.throws(function() {
+          encode(Symbol() as any);
+        });
       }
-
-      function fn(e) {
-        expect(e).to.be.a(TypeError);
-      }
-
-      expect(f('a')).to.throwError(fn);
-      expect(f(true)).to.throwError(fn);
-      expect(f(null)).to.throwError(fn);
-      expect(f(void 0)).to.throwError(fn);
-      expect(f(function() {})).to.throwError(fn);
+      /* eslint-enable @typescript-eslint/no-explicit-any, @typescript-eslint/no-empty-function */
     });
 
     it('should threw error if param is not an integer', function() {
-      function f(v) {
-        return function() {
-          base62.encode(v);
-        };
-      }
-
-      function fn(e) {
-        expect(e).to.be.a(TypeError);
-      }
-
-      expect(f(0.1)).to.throwError(fn);
-      expect(f(NaN)).to.throwError(fn);
-      expect(f(Infinity)).to.throwError(fn);
-      expect(f(-Infinity)).to.throwError(fn);
+      assert.throws(function() {
+        encode(0.1);
+      });
+      assert.throws(function() {
+        encode(NaN);
+      });
+      assert.throws(function() {
+        encode(Infinity);
+      });
+      assert.throws(function() {
+        encode(-Infinity);
+      });
     });
 
     it('should convert to base62 string from integer', function() {
-      expect(base62.encode(0)).to.be('0');
-      expect(base62.encode(61)).to.be('Z');
-      expect(base62.encode(62)).to.be('10');
-      expect(base62.encode(-0)).to.be('0');
-      expect(base62.encode(-61)).to.be('-Z');
-      expect(base62.encode(-62)).to.be('-10');
-      expect(base62.encode(2147483647)).to.be('2lkCB1');
-      expect(base62.encode(-2147483648)).to.be('-2lkCB2');
+      assert(encode(0) === '0');
+      assert(encode(61) === 'Z');
+      assert(encode(62) === '10');
+      assert(encode(-0) === '0');
+      assert(encode(-61) === '-Z');
+      assert(encode(-62) === '-10');
+      assert(encode(2147483647) === '2lkCB1');
+      assert(encode(-2147483648) === '-2lkCB2');
     });
 
+    it('should convert to base62 string from integer with original table', function() {
+      assert(encode(0, originalTable) === '0');
+      assert(encode(61, originalTable) === 'z');
+      assert(encode(62, originalTable) === '10');
+      assert(encode(-0, originalTable) === '0');
+      assert(encode(-61, originalTable) === '-z');
+      assert(encode(-62, originalTable) === '-10');
+      assert(encode(2147483647, originalTable) === '2LKcb1');
+      assert(encode(-2147483648, originalTable) === '-2LKcb2');
+    });
   });
-
 });
