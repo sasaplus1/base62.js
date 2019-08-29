323030d48472fd47df391d0797c937b7984682fb
sasaplus1 <sasaplus1@gmail.com>
2019-08-29 11:32:32 +0900

    add typedoc

---
+++ assets/js/search.js
@@ -0,0 +1,3 @@
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"1":"External module","64":"Function"},"rows":[{"id":0,"kind":1,"name":"\"index\"","url":"modules/_index_.html","classes":"tsd-kind-external-module"},{"id":1,"kind":64,"name":"decode","url":"modules/_index_.html#decode","classes":"tsd-kind-function tsd-parent-kind-external-module","parent":"\"index\""},{"id":2,"kind":64,"name":"encode","url":"modules/_index_.html#encode","classes":"tsd-kind-function tsd-parent-kind-external-module","parent":"\"index\""}]};
